e6a874e728a656efd642b550703d7da6c38f2140
liuzhichao <liu645559030@qq.com>
2016-12-06 19:24:53 +0800

    自定义模板测试

---
+++ app/src/main/java/com/kikyou/mymacapp/Main6Activity.java
@@ -0,0 +1,12 @@
+package com.kikyou.mymacapp;
+
+import android.os.Bundle;
+
+public class Main6Activity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main6);
+	}
+}
