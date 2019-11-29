44929994e7354cd797b4d6b204fa6e08af7be5dc
Liuzhichao <zhichao.liu@appshahe.com>
2019-11-30 00:44:24 +0800

    git test merge commit

--- app/src/main/java/com/kikyou/mymacapp/Main2Activity.java
+++ app/src/main/java/com/kikyou/mymacapp/Main2Activity.java
@@ -2,15 +2,32 @@ package com.kikyou.mymacapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 @Route(path = "/test/two")
 public class Main2Activity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main2);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        gitTest1();
+        gitTest2();
+        gitTest3();
+    }
+
+    private void gitTest1() {
+        Log.e("GIT", "git test 1");
+    }
+
+    private void gitTest2() {
+        Log.e("GIT", "git test 2");
+    }
+
+    private void gitTest3() {
+        Log.e("GIT", "git test 3");
+    }
 }
